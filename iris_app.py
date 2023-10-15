34f4a8481562058a9d074069d61255a5e6f510b7
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2023-10-15 06:21:46 +0200

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -41,10 +41,10 @@ print(f"Accuracy: {accuracy}")  # Accuracy: 0.91
 pickle.dump(clf, open("rf_model.sav", 'wb'))
 
 
-## **************** THE web APP **********************
+## ******************* THE web APP ************************
 # title and description:
 st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into \
+st.markdown('Toy model to play with the iris flowers dataset and classify the three Species into \
      (setosa, versicolor, virginica) based on their sepal/petal \
     and length/width.')
 
