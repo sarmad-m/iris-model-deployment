542fa445388332466c1653058b4c5328fc11fed9
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2023-10-15 05:10:37 +0200

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -1,13 +1,47 @@
 # import necessary packages
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
 
-from prediction import predict_
+
+### *********** THE MODEL ******************
+# random seed
+seed = 42
+# Read original dataset
+iris_df = pd.read_csv("data/iris.csv")
+iris_df.sample(frac=1, random_state=seed)
+
+# selecting features and target data
+X = iris_df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = iris_df[['Species']]
+
+# split data into train and test sets
+# 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=seed, stratify=y)
+
+# create an instance of the random forest classifier
+clf = RandomForestClassifier(n_estimators=100)
+
+# train the classifier on the training data
+clf.fit(X_train, y_train)
+
+# predict on the test set
+y_pred = clf.predict(X_test)
+
+# calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")  # Accuracy: 0.91
+
+# save the model to disk
+pickle.dump(model, open("rf_model.sav", 'wb'))
 
 
-
+## **************** THE web APP **********************
 # title and description:
 st.title('Classifying Iris Flowers')
 st.markdown('Toy model to play to classify iris flowers into \
@@ -32,7 +66,7 @@ st.text('')
 
 # prediction button
 if st.button("Predict type of Iris"):
-    result = predict_([sepal_l, sepal_w, petal_l, petal_w])
+    result = clf.predict([sepal_l, sepal_w, petal_l, petal_w])
     st.text(result[0])
 
 
