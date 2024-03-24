a61c51c649390b7206757144ada4d0848e5f97cc
sarmad-m <63185071+sarmad-m@users.noreply.github.com>
2024-03-24 21:32:00 +0000

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -18,17 +18,15 @@ st.markdown('Toy model to play with the iris flowers dataset and classify the th
 
 # features sliders for the four plant features:
 st.header("Plant Features")
-col1, col2 = st.columns(2)
-
-with col1:
-    st.text("Sepal characteristics")
-    sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
-
-with col2:
-    st.text("Pepal characteristics")
-    petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+st.header("Input Features")
+Pr =  float(st.number_input("Pr"))
+Frate = float(st.number_input("Frate"))
+Favrg = float(st.number_input("Favrg"))
+Time = float(st.number_input("Time"))
+Vtotal = float(st.number_input("Vtotal"))
+Fmax = float(st.number_input("Fmax"))
+Tmax = float(st.number_input("Tmax"))
+SNO = float(st.number_input("SNO"))
 
 st.text('')
 
