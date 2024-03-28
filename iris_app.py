d3fd8fe18b8a4a896992ccc157581f04fca69def
sarmad-m <63185071+sarmad-m@users.noreply.github.com>
2024-03-28 17:21:51 +0000

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -8,38 +8,27 @@ import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
 
+clf = pickle.load(open('rf_model.pkl', 'rb'))
 
-clf = pickle.load(open('rf_model.pkl','rb'))
-
-
-## ******************* THE web APP ************************
-# title and description:
-st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play with the iris flowers dataset and classify the three Species into \
-     (setosa, versicolor, virginica) based on their sepal/petal \
-    and length/width.')
-
+# Title and description
+st.title('Classifying Urine Flowmeter')
 st.header("Input Features")
-Pr = float(st.number_input("Pr"))
-Frate = float(st.number_input("Frate"))
-Favrg = float(st.number_input("Favrg"))
-Time = float(st.number_input("Time"))
-Vtotal = float(st.number_input("Vtotal"))
-Fmax = float(st.number_input("Fmax"))
-Tmax = float(st.number_input("Tmax"))
-SNO = float(st.number_input("SNO"))
 
-# Combine input features into a 1D NumPy array
-input_features = np.array([Pr, Frate, Favrg, Time, Vtotal, Fmax, Tmax, SNO])
+# Input features
+Pr = st.slider("Pr", min_value=0, max_value=100, step=1)
+Frate = st.number_input("Frate")
+Favrg = st.number_input("Favrg")
+Time = st.number_input("Time")
+Vtotal = st.number_input("Vtotal")
+Fmax = st.number_input("Fmax")
+Tmax = st.number_input("Tmax")
+SNO = st.number_input("SNO")
 
-# Reshape the array to be a 2D array with a single row
-input_features = input_features.reshape(1, -1)
-
-# Scale the input features using StandardScaler
+# Scale the input features
 sc = StandardScaler()
-scaled_features = sc.fit_transform(input_features)
+scaled_features = sc.fit_transform([[Pr, Frate, Favrg, Time, Vtotal, Fmax, Tmax, SNO]])
 
-# prediction button
+# Prediction button
 if st.button("Predict"):
     result = clf.predict(scaled_features)
     st.text("Predicted Class:", result[0])
@@ -47,6 +36,3 @@ if st.button("Predict"):
 
 st.text('')
 st.text('')
-st.markdown(
-    '`Initial code was developed by` [santiviquez](https://twitter.com/santiviquez) | \
-         `Code modification and update by:` [Mohamed Alie](https://github.com/Kmohamedalie/iris-streamlit/tree/main)')
