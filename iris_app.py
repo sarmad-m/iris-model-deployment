74d36dc30c5bf0430216bb5862f409d8e5082f65
sarmad-m <63185071+sarmad-m@users.noreply.github.com>
2024-03-30 16:11:31 +0000

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -11,29 +11,18 @@ import joblib
 # Load the pre-trained model
 model = joblib.load("rf_model.pkl")
 
-# Define the columns to scale
-columns_to_scale = ['Pr', 'Frate', 'Favrg', 'Time', 'Vtotal', 'Fmax', 'Tmax', 'SNO']
-
-# Preprocessing functions
-def preprocess_data(data):
-    scaler = StandardScaler()
-    data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
-    # Fill any potential missing values with zeros
-    data = data.fillna(0)
-    return data
-
 # Streamlit app
 st.title("Urine Flow Classification")
 
 # Sidebar for input
 st.sidebar.header("Input Parameters")
 input_params = {}
+columns_to_scale = ['Pr', 'Frate', 'Favrg', 'Time', 'Vtotal', 'Fmax', 'Tmax', 'SNO']
 for col in columns_to_scale:
     input_params[col] = st.sidebar.number_input(f"Enter {col}", value=0.0)
 
 # Prediction
 input_data = pd.DataFrame([input_params])
-input_data = preprocess_data(input_data)
 prediction = model.predict(input_data)[0]
 
 # Display prediction
