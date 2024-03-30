e414da0722ab0617a7de63a9db2114d3bee41a46
sarmad-m <63185071+sarmad-m@users.noreply.github.com>
2024-03-30 15:24:40 +0000

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -3,37 +3,20 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import joblib
 
-# Load data
-train = pd.read_csv("Urineflowmeterdata.csv")
+# Load the pre-trained model
+model = joblib.load("rf_model.pkl")
 
-# Preprocessing
-features = train.loc[:, 'Pr':'SNO']
-scaler = StandardScaler()
-columns_to_scale = ['Pr', 'Frate', 'Favrg', 'Time', 'Vtotal', 'Fmax', 'Tmax', 'SNO']
-features[columns_to_scale] = scaler.fit_transform(features[columns_to_scale])
-classes = train['class']
-le = LabelEncoder()
-classes = le.fit_transform(classes)
-
-# Oversampling
-def oversampling(X_train, y_train):
-    sm = SMOTE(random_state=40, sampling_strategy=dict)
-    x_smote_train, y_smote_train = sm.fit_resample(X_train, y_train)
-    return x_smote_train, y_smote_train
-
-x_smote_train, y_smote_train = oversampling(features, classes)
-
-# Model training
-X_train, X_test, y_train, y_test = train_test_split(x_smote_train, y_smote_train, test_size=0.2, random_state=40)
-rf_classifier = RandomForestClassifier()
-param_grid = {'n_estimators': [50, 100], 'max_depth': [None, 10], 'min_samples_split': [2], 'min_samples_leaf': [1]}
-grid_search = GridSearchCV(estimator=rf_classifier, param_grid=param_grid, cv=10, scoring='neg_log_loss', n_jobs=-1)
-grid_search.fit(X_train, y_train)
-best_rf_model = grid_search.best_estimator_
+# Preprocessing functions
+def preprocess_data(data):
+    scaler = StandardScaler()
+    columns_to_scale = ['Pr', 'Frate', 'Favrg', 'Time', 'Vtotal', 'Fmax', 'Tmax', 'SNO']
+    data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
+    return data
 
 # Streamlit app
 st.title("Urine Flow Classification")
@@ -46,10 +29,9 @@ for col in columns_to_scale:
 
 # Prediction
 input_data = pd.DataFrame([input_params])
-input_data_scaled = scaler.transform(input_data)
-prediction = best_rf_model.predict(input_data_scaled)
-prediction_class = le.inverse_transform(prediction)[0]
+input_data = preprocess_data(input_data)
+prediction = model.predict(input_data)[0]
 
 # Display prediction
 st.write("## Prediction")
-st.write(f"The predicted class is: {prediction_class}")
+st.write(f"The predicted class is: {prediction}")
